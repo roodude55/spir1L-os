fac5cd573af2d0c7aea524b3cad2a4ca8e9f1a8d
nuzer05ive <nuzer05ive@gmail.com>
2025-06-13 06:45:42 -0700

    docs: add audit warning about aframe's got vulnerability to prototype README (P&Q)

--- packages/math/dist/index.js
+++ packages/math/dist/index.js
@@ -1,8 +1,4 @@
 export * from "./constants.js";
 export * from "./prime-harmonic.js";
 export * from "./ni1k.js";
-<<<<<<< HEAD
 export * from "./spir1L-alignment.js";
-=======
-export * from './spir1L-alignment.js';
->>>>>>> e1b08c7a30ee2f9f7dfc1bb169489ca5fd5906c9
---
+++ spiral-os-prototype/README.md
@@ -0,0 +1,1 @@
+**Note:** aframe@1.7.1 pulls in got@9.6.0 (moderate vulnerability). This is safe for prototype/local use; monitor for upstream fixes before production. P&Q!
